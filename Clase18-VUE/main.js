6b9cbb0af18dd1f1cc812f15903f0e48266770ff
EmilianoGDY <emilianoo9g@gmail.com>
2021-10-12 20:43:23 -0300

    clase vue

--- Clase18-VUE/main.js
+++ Clase18-VUE/main.js
@@ -1,11 +1,35 @@
-let app = new Vue({
-    el:"#app",
-    data:{
-        valorModel:"Hola",
-        valorBind:"Hola",
+// let app = new Vue({
+//     el:"#app",
+//     data:{
+//         valorModel:"Hola",
+//         valorBind:"Hola",
 
-        link:"https://docs.google.com/document/d/1O6-2sG0Lm239LR_eE3p7UV39pPf2vNipGkORwH3mTEQ/edit#heading=h.u2jm18f2s8sl",
-        nombreLink:"Doc de Vue",
+//         link:"https://docs.google.com/document/d/1O6-2sG0Lm239LR_eE3p7UV39pPf2vNipGkORwH3mTEQ/edit#heading=h.u2jm18f2s8sl",
+//         nombreLink:"Doc de Vue",
+//         tecnologias:["html", "css", "javascript"],
+//     }
+// }
+// )
+
+
+new Vue({
+    el:"#formVue",
+    data:{
+        tareas:[],
+        nuevaTarea:"",
+    },
+
+    methods:{
+        guardarTarea(){
+            this.tareas.push(this.nuevaTarea),
+            this.nuevaTarea=''
+        }
     }
-}
-)
+
+
+
+
+
+
+
+})
